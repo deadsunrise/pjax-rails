2d2afc0bd19ea37cedbf1d9833166149503654a2
Deadsunrise <jonvega@frozenlabs.com>
2011-05-22 17:42:11 +0200

    clean whitespace and fix typo

--- lib/pjax.rb
+++ lib/pjax.rb
@@ -1,14 +1,14 @@
 module Pjax
   extend ActiveSupport::Concern
-  
+
   included do
     layout ->(c) { pjax_request? ? pjax_layout? : 'application' }
   end
-  
-  private  
+
+  private
     def redirect_pjax_to(action, url = nil)
       new_url = url_for(url ? url : { action: action })
-      
+
       render js: <<-EJS
         if (!window.history || !window.history.pushState) {
           window.location.href = '#{new_url}';
@@ -26,9 +26,9 @@ module Pjax
     def pjax_request?
       env['HTTP_X_PJAX'].present?
     end
-    
+
     def pjax_layout?
-      File.exists?(RAILS_ROOT+"/app/views/layouts/pjax.haml" ? 'pjax' : false
+      File.exists?(Rails.root+"app/views/layouts/pjax.haml") ? 'pjax' : false
     end
 
 end
